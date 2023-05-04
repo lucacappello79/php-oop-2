059a2034f38a1416a14ea1a0546d96583ba73bad
lucacappello79 <postt79@gmail.com>
2023-05-04 17:42:37 +0200

    feat: adding cards

--- index.php
+++ index.php
@@ -23,20 +23,23 @@
     ?>
 
 
-
     <div class="container">
-
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-            </div>
+        <div class="row mt-4">
+            <?php foreach ($products as $item) { ?>
+                <div class="col-3">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $item->getName(); ?></h5>
+                            <p class="card-text">Type: <?= $item->type; ?></p>
+                            <p class="card-text">Price: €<?= $item->getPrice(); ?></p>
+                            <p class="card-text">Year: <?= $item->getYear() ?></p>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
         </div>
-
-
     </div>
 
 
