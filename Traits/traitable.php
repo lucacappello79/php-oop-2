9a5d89cf954b322c52555d953d628aefe0b7ed7a
lucacappello79 <postt79@gmail.com>
2023-05-05 17:28:46 +0200

    feat: adding exception

--- Traits/traitable.php
+++ Traits/traitable.php
@@ -12,5 +12,14 @@ trait Traitable
 
     public function setSharedProperty(string $sharedProperty)
     {
+        $allowedValues = ['available', 'out of stock'];
+
+        if (in_array($sharedProperty, $allowedValues)) {
+
+            $this->sharedProperty = $sharedProperty;
+        } else {
+
+            throw new Exception("Invalid value, please us: 'available', 'out of stock'");
+        }
     }
 }
